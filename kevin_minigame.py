a624857f82187b2ce7d9b212930e99899e6ba8e0
KevinAsian <55142541+KevinAsian@users.noreply.github.com>
2020-01-16 10:23:31 -0500

    Update kevin_minigame.py

--- kevin_minigame.py
+++ kevin_minigame.py
@@ -7,10 +7,79 @@ import math
 WIDTH = 800
 HEIGHT = 600
 GRAVITY = 1
-PLAYER_JUMP_SPEED = 17
+PLAYER_JUMP_SPEED = 15
 PLAYER_HEALTH = 2
 VIEWPORT_MARGIN = 600
-BULLET_SPEED = 4.5
+BULLET_SPEED = 11
+PLAYER_SPEED = 10
+
+class Player(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        # Load a left facing texture and a right facing texture.
+        texture = arcade.load_texture("assets/indiana_jones.png", mirrored=True, scale=0.45)
+        self.textures.append(texture)
+        texture = arcade.load_texture("assets/indiana_jones.png", scale=0.45)
+        self.textures.append(texture)
+
+        # By default, face right.
+        self.set_texture(settings.TEXTURE_RIGHT)
+
+    def update(self):
+        # Check if player should face left or right
+        if self.change_x < 0:
+            self.set_texture(settings.TEXTURE_LEFT)
+        if self.change_x > 0:
+            self.set_texture(settings.TEXTURE_RIGHT)
+
+class KevinMenuView(arcade.View):
+    def on_show(self):
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("Welcome to Kevin's Minigame!", settings.WIDTH/2, settings.HEIGHT//(3/2),
+                         arcade.color.BLACK, font_size=40, anchor_x="center")
+        arcade.draw_text("Press SPACE to Continue", settings.WIDTH/2, settings.HEIGHT/2-75,
+                         arcade.color.GRAY, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        pass
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.SPACE:
+            instruction_view = KevinInstructionView()
+            self.window.show_view(instruction_view)
+
+
+class KevinInstructionView(arcade.View):
+    def on_show(self):
+        arcade.set_background_color(arcade.color.ORANGE_PEEL)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("How to Play:", settings.WIDTH/2, 500,
+                         arcade.color.BLACK, font_size=40, anchor_x="center")
+        arcade.draw_text("Dodge all the obstacles", settings.WIDTH/2, settings.HEIGHT/2 + 100,
+                         arcade.color.BLACK, font_size=18, anchor_x="center")
+        arcade.draw_text("Find the key and open the door", settings.WIDTH/2, settings.HEIGHT/2 + 50,
+                         arcade.color.BLACK, font_size=18, anchor_x="center")
+        arcade.draw_text("there MIGHT be an easter egg", settings.WIDTH/2, settings.HEIGHT/2,
+                         arcade.color.BLACK, font_size=18, anchor_x="center")
+        arcade.draw_text("Good Luck!", settings.WIDTH/2, settings.HEIGHT/2 - 50,
+                         arcade.color.BLACK, font_size=18, anchor_x="center")
+        arcade.draw_text("Click SPACE to Start", settings.WIDTH/2, settings.HEIGHT/2-150,
+                         arcade.color.BLACK, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = KevinView()
+        self.window.show_view(game_view)
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.SPACE:
+            game_view = KevinView()
+            self.window.show_view(game_view)
 
 class KevinView(arcade.View):
     def __init__(self):
@@ -25,19 +94,26 @@ class KevinView(arcade.View):
         self.player_list = None
         self.obstacles_list = None
         self.background = None
+        self.boss_list = None
         self.BEEMOVE = 7
+        self.shipmove = 9
+        self.boundary = 200
+        self.hasjet = False
+        self.key_list = None
         self.bullet_list = None
+        self.jetpack_list = None
+        self.egg_list = None
         # Separate variable that holds the player sprite
         self.frame_count = 0
-
+        self.haskey = False
+        self.keymove = 0
         self.finish_list = None
         self.physics_engine = None
-
+        self.invincible = False
         # self.player = arcade.Sprite(center_x=WIDTH//2, center_y=100)
         
         
-        self.bullets_texture = arcade.make_soft_circle_texture(15, 
-                         arcade.color.BLACK, outer_alpha=255)
+        
         
 
         self.view_bottom = 0
@@ -56,7 +132,16 @@ class KevinView(arcade.View):
         self.wall_list = arcade.SpriteList()
         self.obstacles_list = arcade.SpriteList()
         self.finish_list = arcade.SpriteList()
-        self.player_sprite = arcade.Sprite("assets/indiana_jones.png", 0.5)
+        self.boss_list = arcade.SpriteList()
+        self.key_list = arcade.SpriteList()
+        self.jetpack_list = arcade.SpriteList()
+        self.egg_list = arcade.SpriteList()
+
+        #add egg
+        self.egg = arcade.Sprite("assets/easteregg.png", 0.1)
+        self.egg.center_x = -5500
+        self.egg.center_y = 140
+        self.egg_list.append(self.egg)
         # Add top-left enemy ship
         enemy = arcade.Sprite(":resources:images/space_shooter/playerShip1_green.png", 0.5)
         enemy.center_x = 3000
@@ -78,6 +163,14 @@ class KevinView(arcade.View):
         enemy.angle = 180
         self.enemy_list.append(enemy)
 
+        
+
+        #boss
+        boss = arcade.Sprite("assets/ufoboss.png", 0.5)
+        boss.center_x = -700
+        boss.center_y = 100
+        self.boss_list.append(boss)
+
         #slime
         self.slime = arcade.Sprite(":resources:images/enemies/slimePurple.png", 1)
         self.slime.center_x = 3500
@@ -85,7 +178,7 @@ class KevinView(arcade.View):
         self.obstacles_list.append(self.slime)
         
         #bee
-        self.bee = arcade.Sprite(":resources:images/enemies/bee.png", 0.7)
+        self.bee = arcade.Sprite(":resources:images/enemies/saw.png", 0.7)
         self.bee.center_x = 4000
         self.bee.center_y = 225
         self.obstacles_list.append(self.bee)
@@ -96,11 +189,23 @@ class KevinView(arcade.View):
         self.flag.center_y = 135
         
         # self.player.texture = arcade.make_soft_square_texture(50, arcade.color.BLUE, outer_alpha=255)
+        self.player_sprite = Player()
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 135
         self.player_list.append(self.player_sprite)
-        
-        for x in range(0, 3000, 100):
+        #troll
+        for x in range(-6000, -5000, 100):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", 1)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+        #troll
+        for x in range (-3000, -2000, 100):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", 1)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+        for x in range (-100, 3000, 100):
             wall = arcade.Sprite(":resources:images/tiles/grassMid.png", 1)
             wall.center_x = x
             wall.center_y = 32
@@ -119,6 +224,21 @@ class KevinView(arcade.View):
             wall.center_y = ycor
             ycor += 50
             self.wall_list.append(wall)
+
+        ycor2 = 150
+        for x in range(-2000, -1000, 300):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", 0.7)
+            wall.center_x = x
+            wall.center_y = ycor2
+            ycor2 += 50
+            self.wall_list.append(wall)
+        ycor2 = 250
+        for x in range(-900, -100, 300):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", 0.7)
+            wall.center_x = x
+            wall.center_y = ycor2
+            ycor2 -= 50
+            self.wall_list.append(wall)
         
         for x in range(800, 1800, 400):
             spike = arcade.Sprite(":resources:images/tiles/spikes.png", 1)
@@ -132,6 +252,19 @@ class KevinView(arcade.View):
         door_top = arcade.Sprite(":resources:images/tiles/doorClosed_top.png", 1)
         door_top.center_x = 6000
         door_top.center_y = 230
+
+        #troll
+        self.jetpack = arcade.Sprite("assets/jetpack.png", 0.03)
+        self.jetpack.center_x = -2900
+        self.jetpack.center_y = 150
+        self.jetpack_list.append(self.jetpack)
+
+
+        self.key = arcade.Sprite(":resources:images/items/keyYellow.png", 0.5)
+        self.key.center_x = -2300
+        self.key.center_y = 200
+        self.key_list.append(self.key)
+
         
         self.finish_list.append(door_bot)
         self.finish_list.append(door_top)
@@ -142,7 +275,8 @@ class KevinView(arcade.View):
 
     def on_draw(self):
         arcade.start_render()
-
+        self.egg_list.draw()
+        self.boss_list.draw()
         self.enemy_list.draw()
         self.bullet_list.draw()
         self.flag.draw()
@@ -150,72 +284,89 @@ class KevinView(arcade.View):
         self.obstacles_list.draw()
         self.player_list.draw()
         self.wall_list.draw()
-        
+        self.key_list.draw()
+
+        self.jetpack_list.draw()
         # Draw everything below here.
         
     
     def update(self, delta_time):
-        
-
         self.physics_engine.update()
-        self.slime.center_x -= 7
+        self.player_sprite.update()
+        if self.haskey == True:
+            self.key.center_x = self.player_sprite.center_x
+            self.key.center_y = self.player_sprite.center_y + 70
+        if self.hasjet == True:
+            self.jetpack.center_x = self.player_sprite.center_x
+            self.jetpack.center_y = self.player_sprite.center_y
+        if self.slime.center_x > 0:
+            self.slime.center_x -= 7
         self.bee.center_x -= self.BEEMOVE
         if self.bee.center_x < self.player_sprite.center_x - 150:
             self.BEEMOVE = -15
-        for obstacle in self.obstacles_list:
-            obstacle_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.obstacles_list)
-            if len(obstacle_hit_list) > 0:
-                self.view_left = 5
-                self.player_sprite.center_x = 50
-                self.slime.center_x = 3500
-                self.bee.center_x = 4000
-                self.BEEMOVE = 7
 
-        for finish in self.finish_list:
-            finish_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.finish_list)
-            if len(finish_hit_list) > 0:
-                self.view_left = 5
-                self.player_sprite.center_x = 50
-                self.slime.center_x = 3500
-                self.bee.center_x = 4000
-                self.BEEMOVE = 7
-        if self.player_sprite.center_y < 60:
-            self.view_left = 5
-            self.player_sprite.center_x = 50
-            self.player_sprite.center_y = 135
-            self.slime.center_x = 3500
-            self.bee.center_x = 4000
-            self.BEEMOVE = 7
+        for egg in self.egg_list:
+            egg_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.egg_list)
+            if len(egg_hit_list) > 0:
+                self.invincible = True
+                
+        if self.invincible == True:
+            self.egg.center_x = self.player_sprite.center_x
+            self.egg.center_y = self.player_sprite.center_y
+        #check if invincible
+        if self.invincible == False:
+            for obstacle in self.obstacles_list:
+                obstacle_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.obstacles_list)
+                if len(obstacle_hit_list) > 0:
+                    self.view_left = 5
+                    self.player_sprite.center_x = 50
+                    self.slime.center_x = 3500
+                    self.bee.center_x = 4000
+                    self.BEEMOVE = 7
+                    
 
-        self.frame_count += 1
-        for enemy in self.enemy_list:
+            for key in self.key_list:
+                key_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.key_list)
+                if len(key_hit_list) > 0:
+                    
+                    self.haskey = True
+            for jet in self.jetpack_list:
+                jet_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.jetpack_list)
+                if len(jet_hit_list) > 0:
+                    self.hasjet = True
+        
+            
+            self.frame_count += 1
+            for enemy in self.enemy_list:
 
-            # First, calculate the angle to the player. We could do this
-            # only when the bullet fires, but in this case we will rotate
-            # the enemy to face the player each frame, so we'll do this
-            # each frame.
+                
+                if self.frame_count % 35 == 0:
+                    self.shipangle = random.randrange(220, 300)
+                    bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png")
+                    bullet.center_x = enemy.center_x
+                    
+                    bullet.angle = -90
+                    bullet.change_y = -BULLET_SPEED
+                    bullet.top = enemy.bottom
+                    
+                    self.bullet_list.append(bullet)
 
-            # Position the start at the enemy's current location
-            start_x = enemy.center_x
-            start_y = enemy.center_y
+            
+            for boss in self.boss_list:
 
-            # Get the destination location for the bullet
-            dest_x = self.player_sprite.center_x
-            dest_y = self.player_sprite.center_y
+                start_x = boss.center_x
+                start_y = boss.center_y
 
-            # Do math to calculate how to get the bullet to the destination.
-            # Calculation the angle in radians between the start points
-            # and end points. This is the angle the bullet will travel.
-            x_diff = dest_x - start_x
-            y_diff = dest_y - start_y
-            angle = math.atan2(y_diff, x_diff)
+                dest_x = self.player_sprite.center_x
+                dest_y = self.player_sprite.center_y
 
-            # Set the enemy to face the player.
-            enemy.angle = math.degrees(angle)-90
+                x_diff = dest_x - start_x
+                y_diff = dest_y - start_y
+                angle = math.atan2(y_diff, x_diff)
 
-            # Shoot every 60 frames change of shooting each frame
-            if(self.player_sprite.center_x > 2000):
-                if self.frame_count % 60 == 0:
+                
+                # Shoot every 60 frames change of shooting each frame
+                if self.frame_count % 40 == 0 and self.player_sprite.center_x < 0:
                     bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png")
                     bullet.center_x = start_x
                     bullet.center_y = start_y
@@ -227,17 +378,16 @@ class KevinView(arcade.View):
                     # and change_y. Velocity is how fast the bullet travels.
                     bullet.change_x = math.cos(angle) * BULLET_SPEED
                     bullet.change_y = math.sin(angle) * BULLET_SPEED
-
                     self.bullet_list.append(bullet)
-
-            # Get rid of the bullet when it flies off-screen
+                    
             for bullet in self.bullet_list:
-                if bullet.top < 0:
+                if bullet.center_y < 0:
                     bullet.remove_from_sprite_lists()
 
             self.bullet_list.update()
+            
 
-            #bullet collision
+                #bullet collision
             for bullet in self.bullet_list:
                 bullet_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.bullet_list)
                 if len(bullet_hit_list) > 0:
@@ -246,17 +396,32 @@ class KevinView(arcade.View):
                     self.slime.center_x = 3500
                     self.bee.center_x = 4000
                     self.BEEMOVE = 7
+        for finish in self.finish_list:
+            finish_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.finish_list)
+            if len(finish_hit_list) > 0 and self.haskey == True:
+                self.director.next_view()
                 
-
+        if self.player_sprite.center_y < 50:
+                self.view_left = 5
+                self.player_sprite.center_x = 50
+                self.player_sprite.center_y = 135
+                self.slime.center_x = 3500
+                self.bee.center_x = 4000
+                self.BEEMOVE = 7
+                
+        if self.player_sprite.center_x > 50:
+            self.boundary = 0
+        else:
+            self.boundary = 200
 
          # Scroll left
-        left_boundary = self.view_left + VIEWPORT_MARGIN
+        left_boundary = self.view_left + VIEWPORT_MARGIN 
         if self.player_sprite.left < left_boundary:
             self.view_left -= left_boundary - self.player_sprite.left
             changed = True
 
         # Scroll right
-        right_boundary = self.view_left + WIDTH - VIEWPORT_MARGIN
+        right_boundary = self.view_left + WIDTH - VIEWPORT_MARGIN + self.boundary
         if self.player_sprite.right > right_boundary:
             self.view_left += self.player_sprite.right - right_boundary
             changed = True
@@ -276,13 +441,19 @@ class KevinView(arcade.View):
 
 
     def on_key_press(self, key, key_modifiers):
-        if key == arcade.key.SPACE or key == arcade.key.W:
-            if self.physics_engine.can_jump():
+        if key == arcade.key.SPACE or key == arcade.key.W or key == arcade.key.UP:
+            if self.hasjet == True and self.player_sprite.center_x < 0:
                 self.player_sprite.change_y = PLAYER_JUMP_SPEED
+            else:
+                if self.physics_engine.can_jump():
+                    self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                
         elif key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = -10
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 10
+        elif key == arcade.key.CAPSLOCK and key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 20
 
         """
         Called whenever a key on the keyboard is pressed.
@@ -296,6 +467,7 @@ class KevinView(arcade.View):
             self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
+        
         """
         Called whenever the user lets off a previously pressed key.
         """
@@ -308,15 +480,8 @@ class KevinView(arcade.View):
         pass
 
     def on_mouse_press(self, x, y, button, key_modifiers):
-        bullet = arcade.Sprite()
-        bullet.center_x = self.player.center_x
-        bullet.center_y = self.player.center_y
-        bullet.change_y = 5 
-        bullet.texture = self.bullets_texture
-        bullet.width = 5
-
-        self.bullets_list.append(bullet)
-
+        
+        pass
     def on_mouse_release(self, x, y, button, key_modifiers):
         """
         Called when a user releases a mouse button.
@@ -333,9 +498,10 @@ class KevinView(arcade.View):
 if __name__ == "__main__":
     from utils import FakeDirector
     window = arcade.Window(settings.WIDTH, settings.HEIGHT)
-    my_view = KevinView()
+    my_view = KevinMenuView()
     my_view.director = FakeDirector(close_on_next_view=True)
     window.show_view(my_view)
     arcade.run()
     # main()
-    
+
+
